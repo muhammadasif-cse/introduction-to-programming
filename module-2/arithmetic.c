60cbf40415a8365f9e5c1570120b0244818516ed
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-04-06 23:11:08 +0600

    implement basic arithmetic operations in arithmetic.c

--- module-2/arithmetic.c
+++ module-2/arithmetic.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
-int main ()
+
+int main()
 {
-  return 0;
+    // get input data
+    int const firstNum;
+    int const secondNum;
+    float const firstDivNum;
+    float const secondDivNum;
+    scanf("%d %f", &firstNum, &firstDivNum);
+    scanf("%d %f", &secondNum, &secondDivNum);
+
+    // (+) summation
+    int const sum = firstNum + secondNum;
+    printf("Summation: %d\n", sum);
+
+    // (-) subtraction
+    if (firstNum > secondNum)
+    {
+        int const sub = firstNum - secondNum;
+        printf("Subtraction: %d\n", sub);
+    }
+    else
+    {
+        printf("Subtraction: Please provide the first number greater than the second number!\n");
+    }
+
+    // (*) multiplication
+    int const mul = firstNum * secondNum;
+    printf("Multiplication: %d\n", mul);
+
+    // (/) division
+    float const div = firstDivNum / secondDivNum;
+    printf("Division: %f\n", div);
+
+    // (%) remainder
+    int const rem = firstNum % secondNum;
+    printf("Remainder: %d\n", rem);
+    return 0;
 };
