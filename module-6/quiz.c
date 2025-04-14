a838664fb45cec9e4553206fcaeebf80794bf847
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-04-14 21:57:36 +0600

    add quiz.c to include programming quizzes for loop and conditional logic practice

---
+++ module-6/quiz.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+int main()
+{
+    //* quiz-1 (How many times “hi” will be printed for the following code snippet?)
+    // for (int i = 0; i <= 10; i++)
+    // {
+    //     printf("hi ");
+    //     if (i > 2)
+    //     {
+    //         break;;
+    //     }
+    // }
+
+    //* quiz-2 (How many times (*) will be printed for the following nested loops?)
+    // int i = 3;
+    // while (i <= 5)
+    // {
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         printf("*");
+    //     }
+    //     i++;
+    // }
+
+    //* quiz-3 (How many times “#” will be printed for the following code snippet?)
+    // int i = -3;
+    // while (i < 0)
+    // {
+    //     for (int j = 0; j < 5; j++)
+    //     {
+    //         printf("#");
+    //     }
+    //     i--;
+    // }
+    //* quiz-4 (How many times “hi” will be printed for the following code snippet?)
+    // for (int i = -4; i >= 0; i++)
+    // {
+    //     for (int j = 5; j < 2; j++)
+    //     {
+    //         printf("hi ");
+    //     }
+    // }
+
+    //* quiz-5 (Which one is an infinite loop?)
+    // for (int i = 4; i >= 0; i--) printf("%d", i);
+    // for (int i = 4; i <= 0; i++) printf("%d", i);
+    // for (int i = -4; i <= 0; i--) printf("%d", i);
+
+    //* quiz-6 (Write a loop that can print all values from 1 to 10 inclusive.)
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     printf("%d", i);
+    // }
+
+    //* quiz-7 (Write a loop that can print all values from 1 to 10 exclusive.)
+    // int i = 1;
+    // while (i < 10)
+    // {
+    //     printf("%d", i);
+    //     i++;
+    // }
+
+    //* quiz-8 (What will be the output of the following code snippet?)
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     if (i == 3)
+    //     {
+    //         continue;
+    //     }
+    //     printf("%d ", i);
+    // }
+
+    //* quiz-9 (How many times “hi” will be printed for the following code snippet?)
+    // for (int i = 0; i <= 10; i++)
+    // {
+    //     if (i > 2)
+    //     {
+    //         break;
+    //     }
+    //     printf("hi ");
+    // }
+
+    //* quiz-10 (How many times “hi” will be printed for the following code snippet?)
+    // for (int i = 0; i <= 10; i++)
+    // {
+    //     if (i > 2)
+    //     {
+    //         continue;
+    //     }
+    //     printf("hi ");
+    // }
+    return 0;
+}
