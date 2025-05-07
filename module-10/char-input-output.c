3d28058f11b7fba7291fa6a2f010b7bce9c7e6d4
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-05-08 01:15:32 +0600

    fix: correct output formatting for string input and clarify string length comments

--- module-10/char-input-output.c
+++ module-10/char-input-output.c
@@ -19,10 +19,15 @@ int main()
     //! Reads up to 99 characters or until a newline is encountered
     char m[100];
     fgets(m, 100, stdin);
-    printf("%s", m);
+    printf("%s\n", m);
 
     // char a[100];
     // gets(a);
     // printf("%s", a);
+
+    //* string length
+    // strlen() function returns the length of the string excluding the null character
+    printf("%d\n", sizeof(m));
+    printf("%d\n", strlen(w));
     return 0;
 }
