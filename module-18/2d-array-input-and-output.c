7dfa62062faac6ff0be03b77341cf66d4c996af6
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-05-12 23:21:52 +0600

    add: implement 2D array input and output functionality

---
+++ module-18/2d-array-input-and-output.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int row, col;
+
+    scanf("%d %d", &row, &col);
+
+    int arr[row][col];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("%d\n", arr[i][j]);
+        }
+    }
+
+    return 0;
+}
